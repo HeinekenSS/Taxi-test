c59a6a4eb4942f50d2192c0f6b971a04186e9337
HeinekenSS <heineken2202@gmail.com>
2018-02-04 03:29:12 +0300

    attempt
    one

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var link = document.querySelector('.btn-comment');
+var text = document.querySelector('textarea');
+
+link.addEventListener('click', function (event) {
+    event.preventDefault();
+    text.classList.toggle('open');
+});
+    $(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() != 0) {
+            $('#up-top').fadeIn();
+        } else {
+            $('#up-top').fadeOut();
+        }
+    });
+    $('#up-top').click(function () {
+        $('body,html').animate({scrollTop: 0}, 600);
+    });
+});
